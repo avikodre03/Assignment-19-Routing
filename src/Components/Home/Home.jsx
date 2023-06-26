30e3e77585fd66e28691cceee9e70a7c32791775
avikodre03 <akodre111@gmail.com>
2023-06-26 15:47:47 +0530

    Add To Cart Done 1

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import "./home.css"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+
+      <div className="home">
+
+        <h1>Welcome To React</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem magnam laudantium ipsam? Dolores error earum possimus, nam atque tempore placeat maiores quis, animi beatae sequi quia. Laborum voluptatum cupiditate corporis minima, optio voluptatem nobis deleniti modi pariatur earum sapiente facilis.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem magnam laudantium ipsam? Dolores error earum possimus, nam atque tempore placeat maiores quis, animi beatae sequi quia. Laborum voluptatum cupiditate corporis minima.
+          Dolores error earum possimus, nam atque tempore placeat maiores quis, animi beatae sequi quia. Laborum voluptatum cupiditate corporis minima.
+        </p>
+        <button>Explore More!!!</button>
+      </div>
+    </>
   )
 }
 
